55e2f4de04082febc9052e0e733ac6d91843a6f7
yosoybart <yosoybart03@gmail.com>
2024-10-14 14:18:39 -0400

    Operativo

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { chromium } = require('playwright')
 ;(async () => {
     const browser = await chromium.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.apple.com/la/iphone/')
-    await page.screenshot({path: 'apple-iphone.png'  })
+    await page.goto('https://www.apple.com/shop/buy-iphone/iphone-15')
+    const iPhone15precio =  await page.textContent('[class="nowrap"]')
+    const iPhone15color = await page.textContent('[class="colornav-label"]')
+    console.log({iPhone15precio, iPhone15color})
 
     await browser.close()
 })()
